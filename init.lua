31420bf0e92ddcf146a2b1ca53a2422e49758519
Austin Shenk <ashenkster69@gmail.com>
2013-04-25 00:37:18 -0400

    Added tool break warning

--- init.lua
+++ init.lua
@@ -19,9 +19,14 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 		end
 	end
 end)
+local function removeWarning(name, id)
+    local player = minetest.env:get_player_by_name(name)
+    if player == nil then return end
+    player:hud_remove(id)
+end
 local node_dig = minetest.node_dig
 function minetest.node_dig(pos, node, digger)
-	if digger == nil then return end
+    if digger == nil then return end
 	local pname = digger:get_player_name()
 	local tool = digger:get_wielded_item()
 	local name = nil;
@@ -40,5 +45,19 @@ function minetest.node_dig(pos, node, digger)
 				return
 			end
 		end
+	else if digger:get_wielded_item():get_wear()/65535 > .8 then
+		if not digger:hud_add then return end
+		local id = digger:hud_add({
+			hud_elem_type = "image",
+			position = {x=0.5, y=0.6},
+			name = "Warning",
+			scale = {x=2,y=2},
+			text = "building_warning.png",
+			alignment = {x=0, y=0},
+		})
+		minetest.after(1.0, removeWarning, pname, id)
+		end
 	end
 end
+
+
